0a8adaea1410fe22f5a04dab15fb134383a2fd84
BenDouche <1011024046@qq.com>
2019-11-05 20:40:14 +0800

    Add DataConfig

---
+++ src/cdms/config/DataConfig.java
@@ -0,0 +1,18 @@
+package cdms.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 
+ * 数据库配置类
+ * 
+ * @author fengdouxin
+ * @version 1.0
+ *
+ */
+@Configuration
+public class DataConfig {
+	
+	
+
+}
